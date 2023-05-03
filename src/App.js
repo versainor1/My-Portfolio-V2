951eadd19fea8d5849bb3b1ebc2a754341580099
jennatools-alt <kevingomez554@gmail.com>
2023-05-03 15:21:05 -0700

    Added the rest of the files needed for Portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function App() {
-  return "App";
+    return ( <
+        main >
+        <
+        Navbar / >
+        <
+        About / >
+        <
+        Projects / >
+        <
+        Skills / >
+        <
+        Testimonials / >
+        <
+        Contact / >
+        <
+        /main>
+    );
 }
